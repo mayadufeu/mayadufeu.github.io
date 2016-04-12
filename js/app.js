b333acad75987354e6269db940691eb1b0929b5d
jhhbrook <jhhbrook@gmail.com>
2016-04-12 23:11:09 +0100

    fixed slider jquery to fit all projects, project height now determined by image size (manual)

--- js/app.js
+++ js/app.js
@@ -7,114 +7,35 @@ $(document).mousemove(function(e){
 });
 */
 
-// project heights
-
-// slick: project-1
+// slick
+$('.project > .right').click(function(){
+    $(this).next('.slides').slick('slickNext');
+});
+$('.project > .left').click(function(){
+    $(this).nextAll('.slides').slick('slickPrev');
+});
 $(document).ready(function(){
-    $('.project-1 > .slides').slick({
+    $('.slides').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: '.project-1 > .right',
-        prevArrow: '.project-1 > .left'
+        // cssEase: 'ease-in'
     });
 });
 
-// slick: project-2
-$(document).ready(function(){
-    $('.project-2 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-2 > .right',
-        prevArrow: '.project-2 > .left'
-    });
+// project heights
+var p1w = $('.project-1').width();
+var p2w = $('.project-2').width();
+var p3w = $('.project-3').width();
+var p4w = $('.project-4').width();
+var p5w = $('.project-5').width();
+var p6w = $('.project-6').width();
+var p7w = $('.project-7').width();
+var p8w = $('.project-8').width();
+var p9w = $('.project-9').width();
+$('.project-1').css({
+    'height': p1w * 0.665
 });
-
-// slick: project-3
-$(document).ready(function(){
-    $('.project-3 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-3 > .right',
-        prevArrow: '.project-3 > .left'
-    });
-});
-
-// slick: project-4
-$(document).ready(function(){
-    $('.project-4 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-4 > .right',
-        prevArrow: '.project-4 > .left'
-    });
-});
-
-// slick: project-5
-$(document).ready(function(){
-    $('.project-5 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-5 > .right',
-        prevArrow: '.project-5 > .left'
-    });
-});
-
-// slick: project-6
-$(document).ready(function(){
-    $('.project-6 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-6 > .right',
-        prevArrow: '.project-6 > .left'
-    });
-});
-
-// slick: project-7
-$(document).ready(function(){
-    $('.project-7 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-7 > .right',
-        prevArrow: '.project-7 > .left'
-    });
-});
-
-// slick: project-8
-$(document).ready(function(){
-    $('.project-8 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-8 > .right',
-        prevArrow: '.project-8 > .left'
-    });
-});
-
-// slick: project-9
-$(document).ready(function(){
-    $('.project-9 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-9 > .right',
-        prevArrow: '.project-9 > .left'
-    });
-});
-
-// slick: project-10
-$(document).ready(function(){
-    $('.project-10 > .slides').slick({
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: '.project-10 > .right',
-        prevArrow: '.project-10 > .left'
-    });
+$('.project-2').css({
+    'height': p2w * 1.235
 });
