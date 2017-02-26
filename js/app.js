877816bd473720fa1a635fb600cfe5717ff0fd5b
hugobrook <jhhbrook@gmail.com>
2017-02-26 19:21:28 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
 $(document).foundation();
 
-/* cursor svg
-$(document).mousemove(function(e){
-    $("#cursor").css({left:e.pageX, top:e.pageY});
-    $("#cursor").css('opacity', 1);
-});
-*/
-
 // slick
 $('.project > .right').click(function(){
     $(this).next('.slides').slick('slickNext');
@@ -18,24 +11,92 @@ $(document).ready(function(){
     $('.slides').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        // cssEase: 'ease-in'
+        slidesToScroll: 1
     });
 });
 
-// project heights
-var p1w = $('.project-1').width();
-var p2w = $('.project-2').width();
-var p3w = $('.project-3').width();
-var p4w = $('.project-4').width();
-var p5w = $('.project-5').width();
-var p6w = $('.project-6').width();
-var p7w = $('.project-7').width();
-var p8w = $('.project-8').width();
-var p9w = $('.project-9').width();
-$('.project-1').css({
-    'height': p1w * 0.665
-});
-$('.project-2').css({
-    'height': p2w * 1.235
-});
+var projects = [
+    {
+        name: 'bowndling',
+        htw: 0.668,
+        images: 1
+    },
+    {
+        name: 'tsa',
+        htw: 1.379,
+        images: 1
+    },
+    {
+        name: 'converse-installation',
+        htw: 0.768,
+        images: 1
+    },
+    {
+        name: 'beats',
+        htw: 1.309,
+        images: 1
+    },
+    {
+        name: 'logo-gifs',
+        htw: 1.026,
+        images: 1
+    },
+    {
+        name: 'megumis',
+        htw: 0.670,
+        images: 1
+    },
+    {
+        name: 'fader-gif',
+        htw: 0.670,
+        images: 1
+    },
+    {
+        name: 'scents',
+        htw: 0.709,
+        images: 1
+    },
+    {
+        name: 'br',
+        htw: 1.247,
+        images: 1
+    },
+    {
+        name: 'nts',
+        htw: 0.455,
+        images: 0
+    },
+    {
+        name: 'sc',
+        htw: 1.398,
+        images: 0
+    },
+    {
+        name: 'prints',
+        htw: 1.398,
+        images: 0
+    },
+    {
+        name: 'sprite',
+        htw: 0.607,
+        images: 0
+    },
+    {
+        name: 'signage',
+        htw: 0.448,
+        images: 0
+    }
+];
+
+for (var i = 0; i < projects.length; i++) {
+    var project = projects[i];
+    var className = '.project-' + project.name;
+    $(className).css({
+        'height': ($(className).width() * project.htw) + 'px'
+    });
+    for (var j = 0; j < project.images; j++) {
+        $(className).find('.slide-' + project.images[j].toString()).css({
+            'background': "url('./img/projects/" + project.name + "/" + project.images[j].toString() + ".png')"
+        })
+    }
+}
